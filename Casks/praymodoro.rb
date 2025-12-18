ac7b5eeb1f90236bd8eba010a3ea8dfc7561cf37
George Felter <gfelter@Georges-MacBook-Pro.local>
2025-12-18 12:51:43 -0600

    Initial tap setup with Praymodoro cask

---
+++ Casks/praymodoro.rb
@@ -0,0 +1,22 @@
+cask "praymodoro" do
+  version "1.0.2"
+  sha256 "PLACEHOLDER_SHA256"
+
+  url "https://github.com/JohnVonDrashek/praymodoro-forge/releases/latest/download/Praymodoro-#{version}-arm64.dmg"
+  name "Praymodoro"
+  desc "Prayerful Pomodoro timer with Catholic saints as desktop companions"
+  homepage "https://github.com/JohnVonDrashek/praymodoro-forge"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Praymodoro.app"
+
+  zap trash: [
+    "~/Library/Application Support/Praymodoro",
+    "~/Library/Preferences/com.electron.praymodoro.plist",
+    "~/Library/Saved Application State/com.electron.praymodoro.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,26 @@
+# Homebrew Tap
+
+Personal Homebrew tap for my applications.
+
+## Installation
+
+```bash
+brew tap JohnVonDrashek/tap
+```
+
+## Available Casks
+
+### Praymodoro
+
+A prayerful Pomodoro timer with Catholic saints as desktop companions.
+
+```bash
+brew install --cask praymodoro
+```
+
+## Updating
+
+```bash
+brew update
+brew upgrade --cask praymodoro
+```
